f22d49d176cb31d4e0e03ddada1988a712446c29
juju <liyingliu2010@gmail.com>
2019-06-27 18:50:26 +0800

    Update myc.el

--- myc.el
+++ myc.el
@@ -7,6 +7,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'smartparens-config)
 (add-hook 'c-mode-common-hook #'smartparens-mode)
+
+;;need to do this to have clang-format in your machine: sudo apt install clang-format(linux) or brew install clang-format(macOS)
+(require 'clang-format)
+(add-hook 'c-mode-common-hook
+          (function (lambda ()
+                    (add-hook 'before-save-hook
+                              'clang-format-buffer))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;other setting
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
