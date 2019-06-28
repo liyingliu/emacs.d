fa26e94f94c35484ebfeb57c895af3ed74200423
juju <liyingliu2010@gmail.com>
2019-06-28 11:04:34 +0800

    Update myc.el

--- myc.el
+++ myc.el
@@ -14,6 +14,18 @@
           (function (lambda ()
                     (add-hook 'before-save-hook
                               'clang-format-buffer))))
+
+;;to install irony-server: brew install llvm;export LDFLAGS="-L/usr/local/opt/llvm/lib";export CPPFLAGS="-I/usr/local/opt/llvm/include";https://github.com/Sarcasm/irony-mode/issues/167
+;;(require 'irony)
+;;(add-hook 'c++-mode-hook 'irony-mode)
+;;(add-hook 'c-mode-hook 'irony-mode)
+;;(add-hook 'objc-mode-hook 'irony-mode)
+;;(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+;;only the following 3 lines can have drop downbox, if with the above 5 lines dropdown are gone,but autocomplete still there
+(add-hook 'after-init-hook 'global-company-mode)
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;other setting
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
